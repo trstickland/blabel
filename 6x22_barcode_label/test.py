8a675e3f0768e487c678bc162f4b6c7f14ba0e42
ts24 <ts24@sanger.ac.uk>
2022-10-31 15:36:10 +0000

    Tweak for a 12 character barcode, 6x22mm label

--- 6x22_barcode_label/test.py
+++ 6x22_barcode_label/test.py
@@ -5,8 +5,8 @@ from blabel import LabelWriter
 label_writer = LabelWriter("item_template.html",
                            default_stylesheets=("style.css",))
 records= [
-    dict(sample_id="s01", sample_name="Sample 1"),
-    dict(sample_id="s02", sample_name="Sample 2")
+    dict(sample_id="0123456789_1", sample_name="Sample 1"),
+    dict(sample_id="0123456789_2", sample_name="Sample 2")
 ]
 
-label_writer.write_labels(records, target='qrcode_and_label.pdf')
+label_writer.write_labels(records, target='barcode_and_label.pdf')
